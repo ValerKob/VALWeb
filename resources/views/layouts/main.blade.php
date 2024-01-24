6ebfe3e0c395235626d4630fe3fe7fbd5f978923
Val <89907932+ValerKob@users.noreply.github.com>
2024-01-24 19:33:22 +0300

    FVFVF

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -6,9 +6,21 @@
     <meta charset="utf-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
     <link rel="icon" href="../assets/img/favicon.png" type="image/png" />
-    <title>@yield('title')</title>
+    {{-- <title>@yield('title')</title> --}}
+    <title>VALWeb: Веб Студия - По созданию Дизайнов, Разработке, Продвижению и Адаптивности сайтов.</title>
     <meta name="description" content="VALWeb">
     <meta name="keywords" content="">
+    
+    <!-- SEO -->
+    <meta name="description" content="VALWeb - веб студия, предлагающая качественные услуги по созданию дизайнов сайтов, разработке, продвижению и адаптивности под все устройства. Индивидуальные решения для вашего бизнеса.">
+    <meta name="keywords" content="веб студия, создание дизайнов, разработка сайтов, продвижение сайтов, адаптивность, веб-дизайн, веб-разработка">
+    <link rel="canonical" href="https://www.valweb.ru/">
+
+    <meta property="og:title" content="VALWeb: Веб Студия - Создание Дизайнов, Разработка, Продвижение и Адаптивность">
+    <meta property="og:description" content="VALWeb - веб студия, предлагающая качественные услуги по созданию дизайнов сайтов, разработке, продвижению и адаптивности под все устройства. Индивидуальные решения для вашего бизнеса.">
+    <meta property="og:url" content="https://www.valweb.ru/">
+    <meta property="og:image" content="ссылка на изображение, представляющее ваш сайт">
+
 
     <!-- Favicon -->
     <link rel="apple-touch-icon" sizes="180x180" href="./assets/images/favicon/apple-touch-icon.png">
@@ -55,6 +67,22 @@
     <!-- JS WOW -->
     <script src="./assets/js/wow.min.js"></script>
     <script>new WOW().init();</script>
+
+    <!-- SEO -->
+    <script type="application/ld+json">
+        {   
+            "@context": "http://schema.org",
+            "@type": "LocalBusiness",
+            "name": "VALWeb",
+            "url": "https://www.valweb.ru/",
+            "description": "Веб студия в Архангельске, предоставляющая услуги по разработке и продвижению сайтов.",
+            "address": {
+                "@type": "PostalAddress",
+                "addressLocality": "Архангельск",
+                "addressRegion": "Архангельская область"
+            }
+        }
+    </script>
 </body>
 
 </html>
